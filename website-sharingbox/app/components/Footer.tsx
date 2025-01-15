4ecb76baf79079096682ffe19859d4cc2d747461
Max Unterbusch <82529938+maxiloo@users.noreply.github.com>
2025-01-15 17:08:43 +0100

    add: boxes overview page

--- website-sharingbox/app/components/Footer.tsx
+++ website-sharingbox/app/components/Footer.tsx
@@ -3,9 +3,16 @@ export default function Footer() {
       <footer className="w-full bg-gray-900 text-white py-6 mt-9">
         <div className="container mx-auto text-center flex justify-center">
           <p>Smart Giveaway Box.</p>
-          <a className="mx-8" href="/support"><u>Support</u></a>
-          <a href="/about_us_and_faq"><u>About Us</u></a>
+          <a className="mx-3" href="/boxes">
+            <u>Locations</u>
+          </a>
+          <a className="mr-3" href="/support">
+            <u>Support</u>
+          </a>
+          <a href="/about_us_and_faq">
+            <u>About Us</u>
+          </a>
         </div>
       </footer>
-    )
+    );
 }
