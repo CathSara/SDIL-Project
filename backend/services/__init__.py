61b1a6a5d4cf2afbfa0cc4c000624baa0bb16d58
Max Unterbusch <82529938+maxiloo@users.noreply.github.com>
2025-01-21 22:08:45 +0100

    add: web socket for synching scanned and picked items

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,12 +1,24 @@
-from backend.models.database_service import set_box_open_closed
+from flask_socketio import emit # type: ignore
 
+socketio = None
+
+def init_services(socketio_instance):
+    global socketio
+    socketio = socketio_instance
 
 def open_box(box_id):
+    from backend.models.database_service import set_box_open_closed
     set_box_open_closed(box_id, True)
     # TODO notify arduino to open box
 
 
 def close_box(box_id):
+    from backend.models.database_service import set_box_open_closed
     set_box_open_closed(box_id, False)
     # go through items in this box id and set taken by status
     # notify frontend that box has been closed
+
+
+def notify_frontend(item_status):
+    from . import socketio
+    socketio.emit('item_update', {'status': item_status})
