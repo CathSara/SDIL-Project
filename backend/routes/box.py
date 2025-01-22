e9811b5eb07014e59eac2281a03819a023ecb5ab
timheb16 <65227120+timheb16@users.noreply.github.com>
2025-01-22 20:36:06 +0100

    adjust capture.py and add route to box.py

--- backend/routes/box.py
+++ backend/routes/box.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 
 from backend.services import close_box
 
+from backend.services.capture import capture_and_save_image
+
 box_pb = Blueprint('box', __name__)
 
 @box_pb.route('/notify_closed', methods=['GET'])
@@ -10,6 +12,14 @@ def notify_box_closed():
     close_box(box_id)
     
     return jsonify({'message': 'Notified backend that box has been closed.'}), 200
+
+@box_pb.route("/capture", methods=["GET"])
+def capture_image():
+    image_path = capture_and_save_image()
+    if image_path:
+        return jsonify({"message": "Bild erfolgreich aufgenommen", "path": image_path}), 200
+    else:
+        return jsonify({"message": "Fehler beim Aufnehmen des Bildes"}), 500
 
 
 # storage weight change
