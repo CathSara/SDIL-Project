5df5aa045d377a916024d9304c5d51fe05cb682e
Max Unterbusch <82529938+maxiloo@users.noreply.github.com>
2025-01-23 12:22:12 +0100

    fix: remove duplicate function implementation

--- website-sharingbox/app/components/Header.tsx
+++ website-sharingbox/app/components/Header.tsx
@@ -68,17 +68,6 @@ export default function Header() {
     };
   }, []);
 
-  function getCookie(name: string) {
-    const cookies = document.cookie.split("; ");
-    for (const cookie of cookies) {
-      const [key, value] = cookie.split("=");
-      if (key === name) {
-        return value;
-      }
-    }
-    return "";
-  }
-
   function handleItemSelect(itemId: number | null) {
     if (itemId === null) {
       console.warn("No item selected.");
